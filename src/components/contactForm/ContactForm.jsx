d70f7d4d2a15e0694a47a4a32809e25d2c70f37d
kglanos <130902847+kglanos@users.noreply.github.com>
2023-12-21 22:55:26 +0100

    little changes

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -35,36 +35,40 @@ export const ContactForm = () => {
     return (
         <>
         <div className={css.form}>
-            <h1 className={css.form__title}>Phonebook</h1>
-            <form className={css.form__form} onSubmit={handleSubmit}>
-            <label className={css.form__label} htmlFor="name">
-                Name
-            </label>
-            <input
-                type="text"
-                name="name"
-                className={css.form__input}
-                pattern="[A-Za-zÀ-ÖØ-öø-ÿ]+(?:\s[A-Za-zÀ-ÖØ-öø-ÿ]+)?"
-                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                placeholder="Adrian Smith"
-                required
-            />
-            <label className={css.form__label} htmlFor="number">
-                Number
-            </label>
-            <input
-                type="tel"
-                name="number"
-                className={css.form__input}
-                placeholder="+48 123-456-789"
-                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                required
-            />
-            <button className={css.form__btn}type="submit">
-                Add contact
-            </button>
-            </form>
-        </div>
+    <h1 className={css.form__title}>Phonebook</h1>
+    <form className={css.form__form} onSubmit={handleSubmit}>
+        <label className={css.form__label} htmlFor={"name"}>
+            Name
+        </label>
+        <input
+            type="text"
+            name="name"
+            id="name"
+            className={css.form__input}
+            pattern="[A-Za-zÀ-ÖØ-öø-ÿ]+(?:\s[A-Za-zÀ-ÖØ-öø-ÿ]+)?"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            placeholder="Adrian Smith"
+            autoComplete="name"
+            required
+        />
+        <label className={css.form__label} htmlFor={"number"}>
+            Number
+        </label>
+        <input
+            type="tel"
+            name="number"
+            id="number"
+            className={css.form__input}
+            placeholder="+48 123-456-789"
+            autoComplete="tel"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+        />
+        <button className={css.form__btn} type="submit">
+            Add contact
+        </button>
+    </form>
+</div>
         </>
     );
 };
