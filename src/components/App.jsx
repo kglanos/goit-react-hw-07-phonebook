88f780e82c5ce37d6a4c1232aaa8f765f40c18cd
kglanos <130902847+kglanos@users.noreply.github.com>
2023-12-21 20:29:16 +0100

    Update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { ContactList } from '../components/contactList/ContactList';
 import { ContactForm } from '../components/contactForm/ContactForm';
 import { Filter } from '../components/filter/Filter';
-import css from '../index.css';
 
 const App = () => {
   
