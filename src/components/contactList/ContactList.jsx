981e06bd474852b8ceb7b1d5f05c527b30510c9f
kglanos <130902847+kglanos@users.noreply.github.com>
2023-12-23 02:03:58 +0100

    finally working

--- src/components/contactList/ContactList.jsx
+++ src/components/contactList/ContactList.jsx
@@ -4,40 +4,36 @@ import css from './ContactList.module.css';
 import { fetchContacts, deleteAsyncContact } from '../../redux/operations';
 import { selectContacts, selectFilterValue } from '../../redux/selectors';
 
-  export const ContactList = () => {
+export const ContactList = () => {
   const dispatch = useDispatch();
 
   const contacts = useSelector(selectContacts);
   const filterValue = useSelector(selectFilterValue);
 
   const filteredContacts = Array.isArray(contacts)
-  ? contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filterValue.toLowerCase())
-    )
-  : [];
+    ? contacts.filter((contact) =>
+        contact.name.toLowerCase().includes(filterValue.toLowerCase())
+      )
+    : [];
 
   useEffect(() => {
     dispatch(fetchContacts());
   }, [dispatch]);
 
-  const deleteContactById = async contactId => {
-    try {
-      await dispatch(deleteAsyncContact(contactId));
-    } catch (error) {
-      console.error('Error deleting contact:', error.message);
-    }
+  const handleDeleteClick = (contactId) => {
+    dispatch(deleteAsyncContact(contactId));
   };
 
   return (
     <div className={css.contacts}>
       <h2>Contacts</h2>
       <ul className={css.contacts__list}>
-  {filteredContacts.map(contact => (
-    <li className={css.contacts__item} key={JSON.stringify(contact)}>
-      {contact.name} : {contact.phone}{' '}
-      <button onClick={() => dispatch(deleteContactById(contact.id))}>
-        Delete
-      </button>
+        {filteredContacts.map((contact) => (
+          <li className={css.contacts__item} key={JSON.stringify(contact)}>
+            {contact.name} : {contact.phone}{' '}
+            <button onClick={() => handleDeleteClick(contact.id)}>
+              Delete
+            </button>
           </li>
         ))}
       </ul>
--- src/redux/operations.js
+++ src/redux/operations.js
@@ -6,12 +6,12 @@ axios.defaults.baseURL = 'https://6584738a4d1ee97c6bcfc4f4.mockapi.io/kglanos';
     export const fetchContacts = createAsyncThunk(
         'contacts/fetchAll',
         async (_, thunkAPI) => {
-            try {
-                const response = await axios.get('/contacts');
-                return response.data;
-            } catch (error) {
-                return thunkAPI.rejectWithValue(error.message);
-            }
+        try {
+            const response = await axios.get('/contacts');
+            return response.data;
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error.message);
+        }
         }
     );
     
